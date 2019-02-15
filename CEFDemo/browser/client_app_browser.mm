26df7d46b1b721f6d5d80f1f9cb72f46d42bde09
tslearn <tslearn@163.com>
2019-02-15 20:57:45 +0800

    change

--- CEFDemo/browser/client_app_browser.mm
+++ CEFDemo/browser/client_app_browser.mm
@@ -7,77 +7,85 @@
 #include "include/base/cef_logging.h"
 #include "include/cef_cookie.h"
 #include "browser/client_switches.h"
+#include "browser/client_browser.h"
 
 namespace client {
-
-ClientAppBrowser::ClientAppBrowser() {
-  CreateDelegates(delegates_);
-}
-
-void ClientAppBrowser::OnBeforeCommandLineProcessing(
-    const CefString& process_type,
-    CefRefPtr<CefCommandLine> command_line) {
-  // Pass additional command-line flags to the browser process.
-  if (process_type.empty()) {
-    // Pass additional command-line flags when off-screen rendering is enabled.
-    if (command_line->HasSwitch(switches::kOffScreenRenderingEnabled) &&
-        !command_line->HasSwitch(switches::kSharedTextureEnabled)) {
-      // Use software rendering and compositing (disable GPU) for increased FPS
-      // and decreased CPU usage. This will also disable WebGL so remove these
-      // switches if you need that capability.
-      // See https://bitbucket.org/chromiumembedded/cef/issues/1257 for details.
-      if (!command_line->HasSwitch(switches::kEnableGPU)) {
-        command_line->AppendSwitch("disable-gpu");
-        command_line->AppendSwitch("disable-gpu-compositing");
+  
+  ClientAppBrowser::ClientAppBrowser() {
+    browser::CreateDelegates(delegates_);
+    
+  }
+  
+  // static
+  CefRefPtr<CefPrintHandler> ClientAppBrowser::CreatePrintHandler() {
+    return NULL;
+  }
+  
+  void ClientAppBrowser::OnBeforeCommandLineProcessing(
+                                                       const CefString& process_type,
+                                                       CefRefPtr<CefCommandLine> command_line) {
+    // Pass additional command-line flags to the browser process.
+    if (process_type.empty()) {
+      // Pass additional command-line flags when off-screen rendering is enabled.
+      if (command_line->HasSwitch(switches::kOffScreenRenderingEnabled) &&
+          !command_line->HasSwitch(switches::kSharedTextureEnabled)) {
+        // Use software rendering and compositing (disable GPU) for increased FPS
+        // and decreased CPU usage. This will also disable WebGL so remove these
+        // switches if you need that capability.
+        // See https://bitbucket.org/chromiumembedded/cef/issues/1257 for details.
+        if (!command_line->HasSwitch(switches::kEnableGPU)) {
+          command_line->AppendSwitch("disable-gpu");
+          command_line->AppendSwitch("disable-gpu-compositing");
+        }
       }
+      
+      if (command_line->HasSwitch(switches::kUseViews) &&
+          !command_line->HasSwitch("top-chrome-md")) {
+        // Use non-material mode on all platforms by default. Among other things
+        // this causes menu buttons to show hover state. See usage of
+        // MaterialDesignController::IsModeMaterial() in Chromium code.
+        command_line->AppendSwitchWithValue("top-chrome-md", "non-material");
+      }
+      
+      if (!command_line->HasSwitch(switches::kCachePath) &&
+          !command_line->HasSwitch("disable-gpu-shader-disk-cache")) {
+        // Don't create a "GPUCache" directory when cache-path is unspecified.
+        command_line->AppendSwitch("disable-gpu-shader-disk-cache");
+      }
+      
+      DelegateSet::iterator it = delegates_.begin();
+      for (; it != delegates_.end(); ++it)
+        (*it)->OnBeforeCommandLineProcessing(this, command_line);
     }
-
-    if (command_line->HasSwitch(switches::kUseViews) &&
-        !command_line->HasSwitch("top-chrome-md")) {
-      // Use non-material mode on all platforms by default. Among other things
-      // this causes menu buttons to show hover state. See usage of
-      // MaterialDesignController::IsModeMaterial() in Chromium code.
-      command_line->AppendSwitchWithValue("top-chrome-md", "non-material");
-    }
-
-    if (!command_line->HasSwitch(switches::kCachePath) &&
-        !command_line->HasSwitch("disable-gpu-shader-disk-cache")) {
-      // Don't create a "GPUCache" directory when cache-path is unspecified.
-      command_line->AppendSwitch("disable-gpu-shader-disk-cache");
-    }
-
+  }
+  
+  void ClientAppBrowser::OnContextInitialized() {
+    // Register cookieable schemes with the global cookie manager.
+    CefRefPtr<CefCookieManager> manager =
+    CefCookieManager::GetGlobalManager(NULL);
+    DCHECK(manager.get());
+    manager->SetSupportedSchemes(cookieable_schemes_, NULL);
+    
+    print_handler_ = CreatePrintHandler();
+    
     DelegateSet::iterator it = delegates_.begin();
     for (; it != delegates_.end(); ++it)
-      (*it)->OnBeforeCommandLineProcessing(this, command_line);
+      (*it)->OnContextInitialized(this);
   }
-}
-
-void ClientAppBrowser::OnContextInitialized() {
-  // Register cookieable schemes with the global cookie manager.
-  CefRefPtr<CefCookieManager> manager =
-      CefCookieManager::GetGlobalManager(NULL);
-  DCHECK(manager.get());
-  manager->SetSupportedSchemes(cookieable_schemes_, NULL);
-
-  print_handler_ = CreatePrintHandler();
-
-  DelegateSet::iterator it = delegates_.begin();
-  for (; it != delegates_.end(); ++it)
-    (*it)->OnContextInitialized(this);
-}
-
-void ClientAppBrowser::OnBeforeChildProcessLaunch(
-    CefRefPtr<CefCommandLine> command_line) {
-  DelegateSet::iterator it = delegates_.begin();
-  for (; it != delegates_.end(); ++it)
-    (*it)->OnBeforeChildProcessLaunch(this, command_line);
-}
-
-void ClientAppBrowser::OnRenderProcessThreadCreated(
-    CefRefPtr<CefListValue> extra_info) {
-  DelegateSet::iterator it = delegates_.begin();
-  for (; it != delegates_.end(); ++it)
-    (*it)->OnRenderProcessThreadCreated(this, extra_info);
-}
-
+  
+  void ClientAppBrowser::OnBeforeChildProcessLaunch(
+                                                    CefRefPtr<CefCommandLine> command_line) {
+    DelegateSet::iterator it = delegates_.begin();
+    for (; it != delegates_.end(); ++it)
+      (*it)->OnBeforeChildProcessLaunch(this, command_line);
+  }
+  
+  void ClientAppBrowser::OnRenderProcessThreadCreated(
+                                                      CefRefPtr<CefListValue> extra_info) {
+    DelegateSet::iterator it = delegates_.begin();
+    for (; it != delegates_.end(); ++it)
+      (*it)->OnRenderProcessThreadCreated(this, extra_info);
+  }
+  
 }  // namespace client
+
