6b6031932a85123a0fd3ad2435a19e5e628f855e
tslearn <tslearn@163.com>
2019-02-15 21:01:19 +0800

    change

--- CEFDemo/browser/client_app_browser.mm
+++ CEFDemo/browser/client_app_browser.mm
@@ -9,11 +9,50 @@
 #include "browser/client_switches.h"
 #include "browser/client_browser.h"
 
+#include "include/cef_command_line.h"
+#include "include/cef_crash_util.h"
+#include "include/cef_file_util.h"
+#include "browser/client_switches.h"
+
 namespace client {
   
-  ClientAppBrowser::ClientAppBrowser() {
-    browser::CreateDelegates(delegates_);
+  namespace {
+
+    class ClientBrowserDelegate : public ClientAppBrowser::Delegate {
+    public:
+      ClientBrowserDelegate() {}
+      
+      void OnContextInitialized(CefRefPtr<ClientAppBrowser> app) OVERRIDE {
+        if (CefCrashReportingEnabled()) {
+          // Set some crash keys for testing purposes. Keys must be defined in the
+          // "crash_reporter.cfg" file. See cef_crash_util.h for details.
+          CefSetCrashKeyValue("testkey_small1", "value1_small_browser");
+          CefSetCrashKeyValue("testkey_small2", "value2_small_browser");
+          CefSetCrashKeyValue("testkey_medium1", "value1_medium_browser");
+          CefSetCrashKeyValue("testkey_medium2", "value2_medium_browser");
+          CefSetCrashKeyValue("testkey_large1", "value1_large_browser");
+          CefSetCrashKeyValue("testkey_large2", "value2_large_browser");
+        }
+        
+        const std::string& crl_sets_path =
+        CefCommandLine::GetGlobalCommandLine()->GetSwitchValue(
+                                                               switches::kCRLSetsPath);
+        if (!crl_sets_path.empty()) {
+          // Load the CRLSets file from the specified path.
+          CefLoadCRLSetsFile(crl_sets_path);
+        }
+      }
+      
+    private:
+      DISALLOW_COPY_AND_ASSIGN(ClientBrowserDelegate);
+      IMPLEMENT_REFCOUNTING(ClientBrowserDelegate);
+    };
     
+  }  // namespace
+  
+  
+  ClientAppBrowser::ClientAppBrowser() {
+    delegates_.insert(new ClientBrowserDelegate);
   }
   
   // static
