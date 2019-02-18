862c8d6d8afd0a7ac5ab24bc1aaa9db06db0648b
tslearn <tslearn@163.com>
2019-02-18 17:49:09 +0800

    change

--- CEFDemo/browser/client_app_browser.mm
+++ CEFDemo/browser/client_app_browser.mm
@@ -10,50 +10,15 @@
 
 #include "browser/utils.h"
 
-namespace client {
-  namespace {
-    class ClientBrowserDelegate : public ClientAppBrowser::Delegate {
-    public:
-      ClientBrowserDelegate() {}
-      
-      void OnContextInitialized(CefRefPtr<ClientAppBrowser> app) OVERRIDE {
-        if (CefCrashReportingEnabled()) {
-          // Set some crash keys for testing purposes. Keys must be defined in the
-          // "crash_reporter.cfg" file. See cef_crash_util.h for details.
-          CefSetCrashKeyValue("testkey_small1", "value1_small_browser");
-          CefSetCrashKeyValue("testkey_small2", "value2_small_browser");
-          CefSetCrashKeyValue("testkey_medium1", "value1_medium_browser");
-          CefSetCrashKeyValue("testkey_medium2", "value2_medium_browser");
-          CefSetCrashKeyValue("testkey_large1", "value1_large_browser");
-          CefSetCrashKeyValue("testkey_large2", "value2_large_browser");
-        }
-        
-        const std::string& crl_sets_path =
-        CefCommandLine::GetGlobalCommandLine()->GetSwitchValue(
-                                                               switches::kCRLSetsPath);
-        if (!crl_sets_path.empty()) {
-          // Load the CRLSets file from the specified path.
-          CefLoadCRLSetsFile(crl_sets_path);
-        }
-      }
-      
-    private:
-      DISALLOW_COPY_AND_ASSIGN(ClientBrowserDelegate);
-      IMPLEMENT_REFCOUNTING(ClientBrowserDelegate);
-    };
-  }  // namespace
-  
-  ClientAppBrowser::ClientAppBrowser() {
-    delegates_.insert(new ClientBrowserDelegate);
-  }
+namespace client {  
+  ClientAppBrowser::ClientAppBrowser() {}
   
   // static
   CefRefPtr<CefPrintHandler> ClientAppBrowser::CreatePrintHandler() {
     return NULL;
   }
   
-  void ClientAppBrowser::OnBeforeCommandLineProcessing(
-                                                       const CefString& process_type,
+  void ClientAppBrowser::OnBeforeCommandLineProcessing(const CefString& process_type,
                                                        CefRefPtr<CefCommandLine> command_line) {
     // Pass additional command-line flags to the browser process.
     if (process_type.empty()) {
@@ -83,10 +48,6 @@ namespace client {
         // Don't create a "GPUCache" directory when cache-path is unspecified.
         command_line->AppendSwitch("disable-gpu-shader-disk-cache");
       }
-      
-      DelegateSet::iterator it = delegates_.begin();
-      for (; it != delegates_.end(); ++it)
-        (*it)->OnBeforeCommandLineProcessing(this, command_line);
     }
   }
   
@@ -98,25 +59,10 @@ namespace client {
     manager->SetSupportedSchemes(cookieable_schemes_, NULL);
     
     print_handler_ = CreatePrintHandler();
-    
-    DelegateSet::iterator it = delegates_.begin();
-    for (; it != delegates_.end(); ++it)
-      (*it)->OnContextInitialized(this);
   }
   
-  void ClientAppBrowser::OnBeforeChildProcessLaunch(
-                                                    CefRefPtr<CefCommandLine> command_line) {
-    DelegateSet::iterator it = delegates_.begin();
-    for (; it != delegates_.end(); ++it)
-      (*it)->OnBeforeChildProcessLaunch(this, command_line);
+  void ClientAppBrowser::OnBeforeChildProcessLaunch(CefRefPtr<CefCommandLine> command_line) {
+
   }
-  
-  void ClientAppBrowser::OnRenderProcessThreadCreated(
-                                                      CefRefPtr<CefListValue> extra_info) {
-    DelegateSet::iterator it = delegates_.begin();
-    for (; it != delegates_.end(); ++it)
-      (*it)->OnRenderProcessThreadCreated(this, extra_info);
-  }
-  
 }  // namespace client
 
